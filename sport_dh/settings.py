728e6fdb7c5c7cc39ef3c3141f3d47bace0e70be
viet <vietgym007@gmail.com>
2025-12-29 06:43:11 +0700

    feat: migrate data to postgres

--- sport_dh/settings.py
+++ sport_dh/settings.py
@@ -92,10 +92,15 @@ WSGI_APPLICATION = 'sport_dh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'mydb'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432')
     }
 }
+
 
 
 # Password validation
